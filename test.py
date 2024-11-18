9bd82e7fdb8f4a52497a2dec50a21e49e7fc7c94
GMansur7 <gustavo.mansurdeoliveira@hotmail.com>
2024-11-18 13:25:59 -0300

    feat: teste de api com multiplas imagens.

--- test.py
+++ test.py
@@ -2,16 +2,20 @@ import requests
 
 url = "http://localhost:8000/"
 
-file_path = "/home/mansur/Documents/Geovista/yolo_docker/images_10219.jpg"
+# Lista de caminhos para as imagens
+image_paths = [
+    "/home/mansur/Documents/Geovista/yolo_docker/images_10219.jpg",
+    "/home/mansur/Documents/Geovista/yolo_docker/images_3716.jpg",
+    "/home/mansur/Documents/Geovista/yolo_docker/images_36991.jpg"
+]
 
-# Abra a imagem e envie com a requisição POST
-with open(file_path, "rb") as image_file:
-    files = {"image": image_file}
-    response = requests.post(url + "detect", files=files)
+# Loop para enviar cada imagem para a API
+for file_path in image_paths:
+    with open(file_path, "rb") as image_file:
+        files = {"image": image_file}
+        response = requests.post(url + "detect", files=files)
+        print(f"Response for {file_path}: {response.json()}")
 
 # Requisição GET para o endpoint /device-info
 device_info_response = requests.get(url + "device-info").json()
-
-# Imprima as respostas da API
-print(response.json())
 print(device_info_response)
